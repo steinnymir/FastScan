28774bfdd909dbfc26c3e9b6b14156e4d87447e1
Unknown <sagustss@uni-mainz.de>
2019-02-20 18:11:49 +0100

    basic gui

---
+++ utilities/data.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Steinn Ymir Agustsson
+
+    Copyright (C) 2018 Steinn Ymir Agustsson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import os
+import numpy as np
+from multiprocessing import Pool
+
+def bin_dc(data,bins):
+    binned_signal = np.zeros_like(bins)
+    normarray = np.zeros_like(bins)
+    for i in range(data.shape[1]//2):
+        ii = 2*i
+        j = (np.abs(bins - data[0][ii])).argmin()
+        binned_signal[j] += (data[1][ii]+data[1][ii+1])/2
+        normarray[j]+=1
+    return binned_signal/normarray
+
+def bin(data,bins):
+    binned_signal = np.zeros_like(bins)
+    normarray = np.zeros_like(bins)
+    for i in range(data.shape[1]):
+        j = (np.abs(bins - data[0][i])).argmin()
+        binned_signal[j] += data[1][i]
+        normarray[j]+=1
+    return binned_signal/normarray
+
+
+def bin_dc_multi(arguments):
+    data, bins = arguments
+    binned_signal = np.zeros_like(bins)
+    normarray = np.zeros_like(bins)
+    for i in range(data.shape[1]//2):
+        ii = 2*i
+        j = (np.abs(bins - data[0][ii])).argmin()
+        binned_signal[j] += (data[1][ii]+data[1][ii+1])/2
+        normarray[j]+=1
+    return binned_signal, normarray
+
+def bin_multi(arguments):
+    data, bins = arguments
+    binned_signal = np.zeros_like(bins)
+    normarray = np.zeros_like(bins)
+    for i in range(data.shape[1]):
+        j = (np.abs(bins - data[0][i])).argmin()
+        binned_signal[j] += data[1][i]
+        normarray[j]+=1
+    return binned_signal, normarray
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ utilities/qt.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Steinn Ymir Agustsson
+
+    Copyright (C) 2018 Steinn Ymir Agustsson, Vladimir Grigorev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMessageBox
+
+def raise_Qerror(doingWhat, errorHandle, type='Warning', popup=True):
+    """ opens a dialog window showing the error"""
+    errorMessage = 'Thread Error while {0}:\n{1}'.format(doingWhat, errorHandle)
+    print(errorMessage)
+    if popup:
+        errorDialog = QMessageBox()
+        errorDialog.setText(errorMessage)
+        if type == 'Warning':
+            errorDialog.setIcon(QMessageBox.Warning)
+        elif type == 'Critical':
+            errorDialog.setIcon(QMessageBox.Critical)
+        errorDialog.setStandardButtons(QMessageBox.Ok)
+        errorDialog.exec_()
+
+def my_exception_hook(exctype, value, traceback):
+    # Print the error and traceback
+    print(exctype, value, traceback)
+    # Call the normal Exception hook after
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+def recompile(folder):
+    print('recompiling')
+    uic.compileUiDir(folder, execute=True)
+    print('done')
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
