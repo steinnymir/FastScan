fb4423f3dd992527f5360df4e2a1da9b4ba5941c
Unknown <sagustss@uni-mainz.de>
2019-02-21 15:23:27 +0100

    reordering classes

---
+++ gui/mainwindow.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Steinn Ymir Agustsson
+
+    Copyright (C) 2018 Steinn Ymir Agustsson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import numpy as np
+import pyqtgraph as pg
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QHBoxLayout, QVBoxLayout, QPushButton, \
+    QGroupBox, QSpinBox
+from pyqtgraph.Qt import QtCore, QtGui
+
+from gui.threads import Streamer, Thread, Binner
+
+
+class FastScanMainWindow(QMainWindow):
+    _SIMULATE = True
+
+    def __init__(self):
+        super(FastScanMainWindow, self).__init__()
+        self.setWindowTitle('Fast Scan')
+        self.setGeometry(100, 50, 1152, 768)
+
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage('ready')
+
+        # set the cool dark theme and other plotting settings
+        # self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        pg.setConfigOption('background', .1)
+        pg.setConfigOption('foreground', .9)
+        pg.setConfigOptions(antialias=True)
+        self.setupUi()
+
+        #########################
+        #   define variables    #
+        #########################
+        # self.main_clock = QTimer()
+        # self.set_clock_frequency(2)
+        # self.main_clock.timeout.connect(self.on_timer)
+        # self.main_clock_running = False
+
+        self.laser_trigger_frequency = 273000
+        self.shaker_frequency = 10
+        self.n_periods = 2
+
+        self.n_samples = int((self.laser_trigger_frequency / self.shaker_frequency) * self.n_periods)
+
+        self.bins = None
+        self.n_bins = 1000
+        self.bin_cutoff = .02
+
+
+    def setupUi(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        central_layout = QHBoxLayout()
+        central_widget.setLayout(central_layout)
+
+        verticalSpacer = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+
+        control_widget = QWidget()
+        central_layout.addWidget(control_widget)
+        control_layout = QVBoxLayout()
+        control_widget.setLayout(control_layout)
+
+        visual_widget = QWidget()
+        central_layout.addWidget(visual_widget)
+        visual_layout = QGridLayout()
+        visual_widget.setLayout(visual_layout)
+
+        self.top_plot_area = pg.PlotWidget(name='top_plot')
+        visual_layout.addWidget(self.top_plot_area, 0, 0, 1,1)
+        self.top_plot = self.top_plot_area.plot()
+        self.top_plot.setPen(pg.mkPen(255, 255, 255))
+        self.top_plot_area.setLabel('left', 'Value', units='V')
+        self.top_plot_area.setLabel('bottom', 'Time', units='s')
+
+        self.bot_plot_area = pg.PlotWidget(name='top_plot')
+        visual_layout.addWidget(self.bot_plot_area, 1, 0, 1, 1)
+        self.bot_plot = self.bot_plot_area.plot()
+        self.bot_plot.setPen(pg.mkPen(255, 255, 255))
+        self.bot_plot_area.setLabel('left', 'Value', units='V')
+        self.bot_plot_area.setLabel('bottom', 'Time', units='s')
+
+        box_cont = QGroupBox('continuous acquisition')
+        control_layout.addWidget(box_cont)
+        box_cont_layout = QGridLayout()
+        box_cont.setLayout(box_cont_layout)
+
+        self.start_button = QPushButton('start')
+        box_cont_layout.addWidget(self.start_button, 0, 0, 1, 1)
+        self.start_button.clicked.connect(self.start_acquisition)
+        self.stop_button = QPushButton('stop')
+        box_cont_layout.addWidget(self.stop_button, 0, 1, 1, 1)
+        self.stop_button.clicked.connect(self.stop_acquisition)
+        self.stop_button.setEnabled(False)
+
+
+        control_layout.addItem(verticalSpacer)
+
+
+    def make_bins(self, data, n_bins=1000, cutoff=.02):
+        dmax, dmin = data[0].max(), data[0].min()
+        amp = dmax - dmin
+        dmax -= amp * cutoff
+        dmin += amp * cutoff
+        self.bins = np.linspace(dmin, dmax, n_bins)
+
+    # def set_clock_frequency(self, frequency=2):
+    #     self.main_clock.setInterval(1. / frequency)
+    #
+    # def start_stop_timer(self):
+    #     if self.main_clock_running:
+    #         self.main_clock.stop()
+    #         self.main_clock_running = False
+    #         self.status_bar.showMessage('Main Clock Started')
+    #     else:
+    #         self.main_clock.start()
+    #         self.main_clock_running = True
+    #         self.status_bar.showMessage('Main Clock Stopped')
+    #
+    # def on_timer(self):
+    #     shaker_position, signal, n = self.measure()
+    #     self.draw_top_plot(n, shaker_position)
+
+    def start_acquisition(self):
+        self.start_button.setEnabled(False)
+
+        self.status_bar.showMessage('initializing acquisition')
+        self.streamer_thread = Thread()
+        self.streamer_thread.stopped.connect(self.kill_streamer_thread)
+        self.streamer = Streamer(self.n_samples)
+        self.streamer.newData[np.ndarray].connect(self.on_streamer_data)
+        self.streamer.error.connect(self.raise_thread_error)
+        self.streamer.finished.connect(self.on_streamer_finished)
+        self.streamer.moveToThread(self.streamer_thread)
+        self.streamer_thread.started.connect(self.streamer.start_acquisition)
+        self.streamer_thread.start()
+        self.status_bar.showMessage('Acquisition running')
+        self.stop_button.setEnabled(True)
+
+    def stop_acquisition(self):
+        self.status_bar.showMessage('Stopping aquisition')
+        self.stop_button.setEnabled(False)
+        print('attempting to stop thread')
+        self.streamer.stop_acquisition()
+        self.streamer_thread.exit()
+        self.status_bar.showMessage('Ready')
+        self.start_button.setEnabled(True)
+
+    @QtCore.pyqtSlot(np.ndarray)
+    def on_streamer_data(self, data):
+        print('stream data recieved')
+        self.draw_top_plot(np.linspace(0, len(data[0]) - 1, len(data[0])), data[1])
+        # self.bin_data(data)
+
+    def on_streamer_finished(self):
+        print('streamer finished signal recieved')
+
+    def kill_streamer_thread(self):
+        print('streamer Thread finished, deleting instance')
+        self.streamer_thread = None
+        self.streamer = None
+
+    def bin_data(self, data):
+        if self.bins is None:
+            self.make_bins(data)
+        self.binner_thread = Thread()
+        self.streamer_thread.stopped.connect(self.kill_binner_thread)
+        self.binner = Binner(data,self.bins)
+        self.binner.newData[np.ndarray].connect(self.on_binner_data)
+        self.binner.error.connect(self.raise_thread_error)
+        self.streamer.finished.connect(self.on_binner_finished)
+        self.binner.moveToThread(self.binner_thread)
+        self.binner_thread.started.connect(self.binner.work)
+        self.binner_thread.start()
+
+    @QtCore.pyqtSlot()
+    def on_binner_data(self, data):
+        self.draw_bot_plot(self.bins, data)
+
+    def on_binner_finished(self):
+        print('streamer finished signal recieved')
+
+    def kill_binner_thread(self):
+        print('streamer Thread finished, deleting instance')
+        self.binner_thread = None
+        self.binner = None
+
+    def raise_thread_error(self, e):
+        print('Rising error from thread')
+        print(e)
+
+    def draw_top_plot(self, xd, yd):
+        self.top_plot.setData(x=xd, y=yd)
+
+    def draw_bot_plot(self,xd,yd):
+        self.bot_plot.setData(x=xd, y=yd)
+
+    def closeEvent(self, event):
+        # geometry = self.saveGeometry()
+        # self.qsettings.setValue('geometry', geometry)
+        super(FastScanMainWindow, self).closeEvent(event)
+        print('quitted properly')
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ gui/threads.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Steinn Ymir Agustsson
+
+    Copyright (C) 2018 Steinn Ymir Agustsson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import os
+from multiprocessing import Pool
+import time
+
+import nidaqmx
+import numpy as np
+from nidaqmx import stream_readers
+from nidaqmx.constants import Edge, AcquisitionType
+from PyQt5 import QtCore
+
+from utilities.data import bin_dc_multi, bin_dc
+
+class Thread(QtCore.QThread):
+    stopped = QtCore.pyqtSignal()
+    def stop(self):
+        self.threadactive = False
+        self.wait()
+        self.stopped.emit()
+
+class Streamer(QtCore.QObject):
+    finished = QtCore.pyqtSignal()
+    newData = QtCore.pyqtSignal(np.ndarray)
+    error = QtCore.pyqtSignal(OSError)
+
+    def __init__(self, n_samples, iterations=None, simulate=True):
+        super().__init__()
+
+        self.n_samples = n_samples
+        self.iterations = iterations
+        self.data = np.zeros((3, n_samples))
+        self.simulate = simulate
+
+        self.should_stop = True
+
+    @QtCore.pyqtSlot()
+    def start_acquisition(self):
+        if self.simulate:
+            self.start_simulated_acquisition()
+        else:
+            try:
+                with nidaqmx.Task() as task:
+
+                    self.reader = stream_readers.AnalogMultiChannelReader(task.in_stream)
+                    print('reader initialized')
+                    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")  # shaker position chanel
+                    task.ai_channels.add_ai_voltage_chan("Dev1/ai1")  # signal chanel
+                    task.ai_channels.add_ai_voltage_chan("Dev1/ai2")  # dark control chanel
+                    print('added tasks')
+                    task.timing.cfg_samp_clk_timing(1000000, source="/Dev1/PFI0",
+                                                    active_edge=Edge.RISING,
+                                                    sample_mode=AcquisitionType.CONTINUOUS)
+                    task.start()
+                    print('started tasks')
+
+                    self.should_stop = False
+                    i = 0
+                    while True:
+                        i += 1
+                        print('measuring cycle {}'.format(i))
+                        self.measure()
+                        if i>= self.iterations:
+                            self.should_stop = True
+                        if self.should_stop:
+                            print('loop broken, acquisition stopped.')
+                            self.finished.emit()
+                            break
+
+            except Exception as e:
+                self.on_error(e)
+
+    def start_simulated_acquisition(self):
+        self.should_stop = False
+        if self.iterations is None:
+            i = 0
+            while not self.should_stop:
+                i += 1
+                print('measuring cycle {}'.format(i))
+                self.simulate_measure()
+        else:
+            for i in range(self.iterations):
+                self.simulate_measure()
+
+    @QtCore.pyqtSlot()
+    def stop_acquisition(self):
+        print('thread got message to stop')
+        self.should_stop = True
+        print('scheduled to stop')
+
+    def measure(self):
+
+        self.reader.read_many_sample(self.data, number_of_samples_per_channel=self.n_samples)
+        print('reader wrote data: {}'.format(self.data.mean()))
+        self.newData.emit(self.data)
+
+    def simulate_measure(self):
+        time.sleep(.9 * self.n_samples / 300000)
+        n = np.arange(len(self.data[0]))
+        phase = np.random.rand(1) * 2 * np.pi
+        self.data[0, :] = np.cos(n / 10000 + phase)
+        self.data[1, :] = self.data[0, :] / 3
+        self.data[2, :] = np.array([i % 2 for i in range(len(self.data[0]))])
+        self.newData.emit(self.data)
+
+    def on_error(self, e):
+        self.error.emit(e)
+
+
+class Binner(QtCore.QObject):
+    newData = QtCore.pyqtSignal(np.ndarray)
+    error = QtCore.pyqtSignal(list)
+
+    def __init__(self, data, bins):
+        super().__init__()
+        self.data_input = data
+        self.bins = bins
+        self.data_output = np.zeros_like(bins)
+        self.bin_method = bin_dc
+
+    @QtCore.pyqtSlot()
+    def work(self):
+        try:
+            chunks = os.cpu_count() - 2
+            data_split = np.split(self.data_input, chunks, axis=1)
+            args = []
+            for data in data_split:
+                args.append((data, self.bins))
+            pool = Pool(chunks)
+            results = pool.map(self.bin_method, args)
+            results = np.array(results)
+
+            binned_signal = []
+            normarray = []
+
+            for i in range(8):
+                binned_signal.append(results[i, 0, :])
+                normarray.append(results[i, 1, :])
+            binned_signal = np.nansum(np.array(binned_signal), 0)
+            normarray = np.nansum(np.array(normarray), 0)
+
+            self.data_output = binned_signal / normarray
+            print('data binned: emitting results')
+            self.newData.emit(self.data_output)
+        except Exception as e:
+            self.error.emit(list[e])
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
