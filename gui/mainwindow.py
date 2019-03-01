fixed pandas averaging

gui/mainwindow.py
@@ -63,10 +63,12 @@ class FastScanMainWindow(QMainWindow):
 
         self.settings = {'laser_trigger_frequency': 273000,
                          'shaker_frequency': 10,
-                         'n_samples': 100000,
+                         'n_samples': 30000,
                          'shaker_amplitude': 10,
                          'n_plot_points': 15000
                          }
+
+
         self.data = {'processed': None,
                      'unprocessed': np.zeros((3, 0)),
                      'time_axis': None,
@@ -425,20 +427,14 @@ class FastScanMainWindow(QMainWindow):
         signal = data[1]
         signal_df = pd.DataFrame(data=signal,index=time_axis)
 
-        self.data['last_trace'] = signal_df
         if self.data['df_traces'] is None:
-            self.data['df_traces'] = pd.DataFrame(data=signal,index=time_axis)
-            self.data['df_averages'] = self.data['df_traces']
-
+            self.data['df_traces'] = signal_df
         else:
-            signal_df = pd.DataFrame(data=signal, index=time_axis)
             self.data['df_traces'] = pd.concat([self.data['df_traces'],signal_df],axis=1)
-            self.data['df_averages'] = self.data['df_traces'].mean(axis=1)
 
 
         self.draw_main_plot()
-        if self.fit_sech2_checkbox.isChecked() or self.fit_sech2_checkbox.isChecked():
-            self.fit_data(self.current_average)
+
 
     def on_processor_finished(self):
         print('Processor finished signal recieved')
@@ -448,11 +444,11 @@ class FastScanMainWindow(QMainWindow):
         self.processor_thread = None
         self.processor = None
 
-    def fit_data(self, data):
+    def fit_data(self, time_axis, data):
 
         self.fitter_thread = Thread()
         self.fitter_thread.stopped.connect(self.kill_fitter_thread)
-        self.fitter = Fitter(self.time_axis,data)
+        self.fitter = Fitter(time_axis,data)
         self.fitter.newData[np.ndarray].connect(self.on_fitter_data)
         self.fitter.error.connect(self.raise_thread_error)
         self.fitter.finished.connect(self.on_fitter_finished)
@@ -495,24 +491,36 @@ class FastScanMainWindow(QMainWindow):
         self.raw_data_plot.setData(x=xd, y=yd)
 
     def draw_main_plot(self):
-        # if self.data['time_axis'] is None:
-        #     self.make_time_axis()
-        # y = self.data['all_traces'][-1]
-        # x = np.linspace(-self.settings['shaker_amplitude']/2,self.settings['shaker_amplitude']/2,len(y))
 
         current = self.data['df_traces'].values[:,-1]
         x_current = self.data['df_traces'].index.values
-        average = self.data['df_averages'].values[:,0]
-        x_avg = self.data['df_averages'].index.values
-
-        # yavg = np.array(self.data['all_traces']).mean(axis=0)
-        # yavg = self.current_average
         self.plot_back_line.setData(x=x_current[2:-2], y=current[2:-2])
-        self.plot_front_line.setData(x=x_avg[2:-2], y=average[2:-2])
-        if self.peak_fit_data is not None:
-            if self.fit_sech2_checkbox.isChecked() or self.fit_gauss_checkbox.isChecked():
-                yfit = self.peak_fit_data
-                self.plot_fit_line.setData(x=x[2:-2], y=yfit[2:-2])
+
+        x_avg,y_avg = self.get_avg_curve(length=1000,n_averages=0)
+
+        self.plot_front_line.setData(x=x_avg, y=y_avg)
+
+        if self.fit_sech2_checkbox.isChecked() or self.fit_sech2_checkbox.isChecked():
+            self.fit_data(x_avg,y_avg)
+
+
+    def get_avg_curve(self,length=1000,n_averages=0):
+
+        if n_averages == 0:
+            df = self.data['df_traces']
+        else:
+            n_averages = min(len(self.data['df_traces'].columns),n_averages)
+            df = self.data['df_traces'][self.data['df_traces'][:-n_averages]]
+
+        n_plt_pts = length
+        a, b = df.index.min(), df.index.max()
+        bins = np.linspace(a, b, n_plt_pts + 1)
+        step = bins[1] - bins[0]
+        plot_bins = np.linspace(a + step / 2, b - step / 2, n_plt_pts)
+
+        binned = df.groupby(pd.cut(df.index, bins)).mean()
+        avg = binned.mean(axis=1).values
+        return plot_bins, avg
 
 
 

gui/threads.py
@@ -26,11 +26,16 @@ from multiprocessing import Pool
 
 from scipy.optimize import curve_fit
 
-import nidaqmx
+try:
+    import nidaqmx
+    from nidaqmx import stream_readers
+    from nidaqmx.constants import Edge, AcquisitionType
+except:
+    print('no nidaqmx package installed.')
+
 import numpy as np
 from PyQt5 import QtCore
-from nidaqmx import stream_readers
-from nidaqmx.constants import Edge, AcquisitionType
+
 
 from utilities.data import bin_dc_multi, bin_dc
 from utilities.math import gaussian, gaussian_fwhm, sech2_fwhm
@@ -106,6 +111,8 @@ class Streamer(Worker):
 
     def start_simulated_acquisition(self):
         self.should_stop = False
+
+
         if self.iterations is None:
             i = 0
             while not self.should_stop:
@@ -135,7 +142,7 @@ class Streamer(Worker):
         noise = np.random.rand(len(n))
         phase = noise[0] * 2 * np.pi
 
-        amplitude = .5
+        amplitude = .5*(1+.02*np.random.uniform(-1,1))
         self.data[0, :] =  np.cos(2*np.pi*n / 30000 + phase)*amplitude/2 #in volt
 
         # self.data[2, :] = np.array([i % 2 for i in range(len(n))])
@@ -143,7 +150,7 @@ class Streamer(Worker):
             self.data[2, i] = i % 2 # dark control channel filled with 1010...
             self.data[1, i] = self.data[0,i]/3 + 1*np.sin(noise[2])
             if i % 2 == 1:
-                self.data[1, i] += gaussian(self.data[0, i], 0, .1) + noise[i]
+                self.data[1, i] += gaussian(self.data[0, i], 0, .01) + noise[i]
 
         dt = time.time()-t0
         time.sleep(max(self.n_samples/273000 - dt,0))
@@ -220,9 +227,9 @@ class Processor(Worker):
 
         step = 0.000152587890625
         minpos = self.shaker_positions.min()
-        min_t = (minpos/step)*.05
+        min_t = (minpos/step)*.05 # consider 0.05 ps step size from shaker digitalized signal
         maxpos = self.shaker_positions.max()
-        max_t = (minpos/step)*.05
+        max_t = (maxpos/step)*.05
 
         n_points = int((maxpos - minpos) / step)
 